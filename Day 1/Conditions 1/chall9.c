741ffd8cc0a9a073c671f600b37d75056df1089b
Ayoub Ouharda <ayoubouharda33@gmail.com>
2025-09-23 17:38:33 +0200

    lettre verif

---
+++ Day 1/Conditions 1/chall9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void l_verif(char a)
+{
+    if(a > 64 && a < 91 )
+    {
+        printf("C une lettre d'alphabets \n");
+    }
+    else if (a > 96 && a < 123)
+    {
+        printf("C une lettre d'alphabets\n");
+    }
+    else
+    {
+        printf("C pas une lettre \n");
+    }
+}
+int main(void)
+{
+    int c;
+    printf("Veuillez saisir une lettre :\n");
+    scanf("%c",&c);
+    l_verif(c);
+
+    return 0;
+}
