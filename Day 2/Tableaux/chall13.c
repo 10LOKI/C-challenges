73c84b5e699821977d688d6c1fa5751a56d9f9f4
Ayoub Ouharda <ayoubouharda33@gmail.com>
2025-09-24 19:45:19 +0200

    Impaire

---
+++ Day 2/Tableaux/chall13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    printf("Veuillez saisir la taille du tableau :\n");
+    scanf("%d",&n);
+    int Tab[n];
+    printf("Veuillez saisir les valeurs du tableau :\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("T[%d] = ",i+1);
+        scanf("%d",&Tab[i]);
+    }
+    printf("\n\n");
+    printf("Les Elements PAires du tableau :\n");
+    for(int i = 0;i<n;i++)
+    {
+        if(Tab[i] % 2 != 0)
+        {
+            printf("%d\n",Tab[i]);
+        }
+    }
+    return 0;
+}
