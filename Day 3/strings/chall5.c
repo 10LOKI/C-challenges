15fdba3195e322aadfc2859a5c1c25080382bb0a
Ayoub Ouharda <ayoubouharda33@gmail.com>
2025-09-29 10:31:33 +0200

    reverse

---
+++ Day 3/strings/chall5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char str1[20];
+    gets(str1);
+    int l=strlen(str1) , temp ;
+    for(int i=0;i<l/2;i++)
+    {
+        temp = str1[i];
+        str1[i]=str1[l-1-i];
+        str1[l-1-i] = temp;
+    }
+    printf("%s",str1);
+    return 0;
+}
