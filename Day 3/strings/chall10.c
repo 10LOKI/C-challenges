e90e23732d32103f250d9c49ca183cf529b2dbb7
Ayoub Ouharda <ayoubouharda33@gmail.com>
2025-09-29 12:00:11 +0200

    incompleted

---
+++ Day 3/strings/chall10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char str[20];
+    char s_str[20];
+printf("Veuillez saisir le string :\n");
+fgets(str,20,stdin);
+printf("Veuillez saisir la sous string :\n");
+fgets(str,20,stdin);
+
+for(int a = 0;str[a] != '\0';a++)
+{
+
+}
+    return 0;
+}
